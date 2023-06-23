1595092d6cdb8ee825e8c9ddbd7e349a22f71b55
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-06-23 22:09:12 +0200

    Exchanged old main program logic for newly implemented system

--- Entrypoint.cs
+++ Entrypoint.cs
@@ -53,7 +53,7 @@ namespace FastMassPing
             Boolean[] varCheck = { false, false, false, false, false, false };
 
             for (int i = 0; i < args.Length; i++)
-            {   
+            {
                 switch (args[i])
                 {
                     case "--help":
@@ -62,10 +62,13 @@ namespace FastMassPing
                         break;
 
                     case "--start-ip":
-                        try {
+                        try
+                        {
                             startAddress = IPAddress.Parse(args[i + 1]);
                             varCheck[0] = true;
-                        } catch {
+                        }
+                        catch
+                        {
                             Console.WriteLine("Start IP address invalid. Aborting.");
                             return;
                         }
@@ -76,37 +79,48 @@ namespace FastMassPing
                         {
                             endAddress = IPAddress.Parse(args[i + 1]);
                             varCheck[1] = true;
-                        } catch {
+                        }
+                        catch
+                        {
                             Console.WriteLine("End IP address invalid. Aborting.");
                             return;
                         }
                         break;
 
                     case "--port":
-                        try {
+                        try
+                        {
                             port = Convert.ToInt32(args[i + 1]);
                             varCheck[2] = true;
-                        } catch {
+                        }
+                        catch
+                        {
                             Console.WriteLine("Port invalid. Aborting.");
                             return;
                         }
                         break;
 
                     case "--timeout":
-                        try {
+                        try
+                        {
                             timeout = Convert.ToInt32(args[i + 1]);
                             varCheck[3] = true;
-                        } catch {
+                        }
+                        catch
+                        {
                             Console.WriteLine("Timeout invalid. Setting to Default.");
                             timeout = 1000;
                         }
                         break;
 
                     case "--threads":
-                        try {
+                        try
+                        {
                             threadCount = Convert.ToInt32(args[i + 1]);
                             varCheck[4] = true;
-                        } catch (Exception) {
+                        }
+                        catch (Exception)
+                        {
                             Console.WriteLine("Number of Threads invalid. Setting to Default.");
                             threadCount = 16;
                         }
@@ -179,133 +193,23 @@ namespace FastMassPing
                 }
             }
 
-            Int64 addressSpace = AddressCounter.GetAddressSpace(startAddress, endAddress);
-            if (addressSpace <= 0)
+            Console.WriteLine("Searching for addresses with open Port {0} with {1} Threads", port, threadCount);
+            Console.WriteLine("Pinging from {0} to {1}\n", startAddress.ToString(), endAddress.ToString());
+
+            ThreadManager manager = new();
+
+            if (!manager.SetAddressSpace(startAddress, endAddress))
             {
                 Console.WriteLine("End address cannot be smaller or equal to Start address. Aborting.");
                 return;
             }
 
-            IPAddress currentAddress = startAddress;
-            Int32 addit = (int)(addressSpace % threadCount);
-            Int32 mean = (int)Math.Floor((float)addressSpace / threadCount);
-
-            Console.WriteLine("Searching for addresses with open Port {0} with {1} Threads", port, threadCount);
-            Console.WriteLine("Pinging from {0} to {1}  ({2} addresses)\n", startAddress.ToString(), endAddress.ToString(), addressSpace);
-
-            Queue<string> output = new Queue<string>();
-            Queue<string> infoOutput = new Queue<string>();
-            Int32[] status = new Int32[threadCount+2];
-            Thread[] threads = new Thread[threadCount+2];
-
-            for (int i = 0; i < threadCount; i++)
-            {
-                IPAddress currentAddressCopy = currentAddress;
-                Int32 threadNum = i;
-                Int32 increm = 0;
-
-                if (i < addit)
-                    increm = 1;
-
-                threads[i] = new Thread(() => PingThread(new AddressCounter(currentAddressCopy, AddressCounter.IncrementAddress(currentAddressCopy, mean + increm)), port, timeout, threadNum, status, output));
-                threads[i].IsBackground = true;
-                threads[i].Start();
-                currentAddress = AddressCounter.IncrementAddress(currentAddress, mean + increm);
-            }
-
-            threads[threadCount] = new Thread(() => MonitorThread(addressSpace, threadCount, status, infoOutput));
-            threads[threadCount].IsBackground = true;
-            threads[threadCount].Start();
-            threads[threadCount+1] = new Thread(() => OutputThread(output, infoOutput, outputFile, threadCount+1, status));
-            threads[threadCount+1].IsBackground = true;
-            threads[threadCount+1].Start();
-
-            for (int i = 0; i < threadCount; i++)
-            {
-                threads[i].Join();
-            }
-
-            status[threadCount] = 1;
-            status[threadCount + 1] = 1;
-            threads[threadCount].Join();
-            threads[threadCount+1].Join();
+            manager.SetOutput(outputFile);
+            manager.SetThreadConfiguration(port, timeout, threadCount);
+            manager.Begin();
+            manager.Wait();
 
             return;
-
-            }
-
-        static void PingThread(AddressCounter counter, Int32 port, Int32 timeout, Int32 threadNum, Int32[] status, Queue<string> output)
-        {
-            using ConnectionTester connectionTester = new(port, timeout);
-            foreach (IPAddress address in counter)
-            {
-                ConnectionInformation info = connectionTester.Test(address);
-                if (info.reachable)
-                {
-                    lock (output)
-                    {
-                        output.Enqueue(info.ToString());
-                    }
-                }
-                status[threadNum]++;
-            }
-        }
-
-        static void MonitorThread(Int64 addressSpace, Int32 threadNum, Int32[] status, Queue<string> output)
-        {
-            DateTime startTime = DateTime.Now;
-            TimeSpan elapsedTime;
-            TimeSpan eta;
-            Int32 pinged;
-            while (status[threadNum] == 0)
-            {
-                Thread.Sleep(500);
-                pinged = status.Sum();
-                elapsedTime = DateTime.Now.Subtract(startTime);
-                eta = elapsedTime.Multiply((double)addressSpace / Math.Max(pinged, 1)).Subtract(elapsedTime);
-                output.Enqueue(String.Format("Pinged {0} out of {1} addresses  ( {2} Completed )  T: {3}   ETA: {4}   {5}\r",
-                    pinged, addressSpace, ((decimal)pinged/addressSpace).ToString("P"), elapsedTime.ToString(@"dd\.hh\:mm\:ss"), 
-                    eta.ToString(@"dd\.hh\:mm\:ss"), startTime.Add(elapsedTime).Add(eta).ToString("G")));
-            }
-        }
-
-        static void OutputThread(Queue<string> output, Queue<string> infoOutput, string outputFile, Int32 threadNum, Int32[] status)
-        {
-            try
-            {
-                using (StreamWriter outFile = new StreamWriter(outputFile, append: true))
-                {
-                    outFile.AutoFlush = true;
-
-                    while (status[threadNum] == 0)
-                    {
-                        while (output.Count > 0)
-                        {
-                            outFile.WriteLine(output.Dequeue());
-                        }
-
-                        while (infoOutput.Count > 0)
-                        {
-                            Console.Write(infoOutput.Dequeue());
-                        }
-                    }
-                }
-            }
-            catch
-            {
-                while (status[threadNum] == 0)
-                {
-                    while (output.Count > 0)
-                    {
-                        Console.WriteLine(output.Dequeue());
-                    }
-
-                    while (infoOutput.Count > 0)
-                    {
-                        Console.Title = infoOutput.Dequeue();
-                    }
-                }
-            }
         }
     }
 }
