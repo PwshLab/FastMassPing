55efa809fa17bbdaa7a6faba6f6fdf9c0de574b8
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2023-06-23 22:08:34 +0200

    Added alternative invocation with PingController

---
+++ PingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMassPing
+{
+    public static class PingController
+    {
+        public static void RunPing(string startAddress, string endAddress, int port, int timeout, int threadCount, string outputPath)
+        {
+            ThreadManager manager = new ThreadManager();
+            manager.SetAddressSpace(IPAddress.Parse(startAddress), IPAddress.Parse(endAddress));
+            manager.SetOutput(outputPath);
+            manager.SetThreadConfiguration(port, timeout, threadCount);
+            manager.Begin();
+            manager.Wait();
+        }
+    }
+}
